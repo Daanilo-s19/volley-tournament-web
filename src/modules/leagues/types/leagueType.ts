a55b7d0919e14deee62f09dd0afcb81b79a12b82
Yansb <yansbarreiro@gmail.com>
2022-06-14 16:14:36 -0300

    feat: [VT-91] implement classifications page

--- src/modules/leagues/types/leagueType.ts
+++ src/modules/leagues/types/leagueType.ts
@@ -33,3 +33,64 @@ export interface InitLeagueInput {
   horarios: number[];
   intervaloDeDiasUteisEntreTurnos: number;
 }
+
+export interface LeagueClassificationOutput{
+  idEquipe: string,
+  setsGanhos: number,
+  setsPerdidos: number,
+  setsDisputados: number,
+  partidasGanhas: number,
+  partidasPerdidas: number,
+  partidasDisputadas: number,
+  pontuacao: number,
+  pontosAverage: number,
+  setsAverage: number,
+  equipe: {
+    id: string,
+    nome: string,
+    urlBrasao: string,
+    apta: boolean,
+    descricaoAptidao: string[],
+    cidade: string,
+    estado: string,
+    idLiga: string,
+    idGinasio: string,
+    tecnico: {
+      id: string,
+      documentoCref: string,
+      idEquipe: string,
+      nome: string,
+      documento: string,
+      genero: "feminino" | "masculino",
+      idade: number,
+      documentoCbv: string
+    },
+    quantidadeAtletas: number,
+    quantidadeAuxiliares: number,
+    atletas: [
+      {
+        id: string,
+        numero: number,
+        idEquipe: string,
+        nome: string,
+        documento: string,
+        genero: "feminino" | "masculino",
+        idade: number,
+        documentoCbv: string
+      }
+    ],
+    auxiliares: [
+      {
+        id: string,
+        idEquipe: string,
+        documentoCref: string,
+        tipoAuxiliar: string,
+        nome: string,
+        documento: string,
+        genero: "feminino" | "masculino",
+        idade: number,
+        documentoCbv: string
+      }
+    ]
+  }
+}
