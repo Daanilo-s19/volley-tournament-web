0f9e388f3cfb0902c1cd1419cff392410b57ded5
Lucas Santana <lcssantana03@gmail.com>
2022-06-29 21:50:02 -0300

    feat: fetch matches in register statistics

--- src/modules/statistics/hooks/useRegisterStatistics.ts
+++ src/modules/statistics/hooks/useRegisterStatistics.ts
@@ -7,7 +7,7 @@ import parseResponseData from "../../../utils/parsers";
 function useRegisterStatistics() {
   const [leagueId, setLeagueId] = useState<string>();
 
-  const { data: league } = useQuery<League>(
+  const { data: league, isLoading: isLoadingLeague } = useQuery<League>(
     ["league", { id: leagueId }],
     () => {
       return api.get(`/liga/${leagueId}`).then(parseResponseData);
@@ -15,7 +15,17 @@ function useRegisterStatistics() {
     { enabled: !!leagueId }
   );
 
-  return { leagueId, setLeagueId, league };
+  const { data: matches, isLoading: isLoadingMatches } = useQuery(
+    ["matches", { leagueId: leagueId }],
+    () => api.get("/partida", { params: { idLiga: leagueId } }),
+    {
+      onSuccess: (d) => console.log(d),
+
+      enabled: !!leagueId && !!league,
+    }
+  );
+
+  return { leagueId, setLeagueId, league, isLoadingLeague };
 }
 
 export { useRegisterStatistics };
--- src/modules/statistics/pages/register.tsx
+++ src/modules/statistics/pages/register.tsx
@@ -1,13 +1,21 @@
-import { Box } from "@chakra-ui/react";
+import { Box, CircularProgress, Flex, Select } from "@chakra-ui/react";
 import SelectLeague from "../../leagues/components/selectLeague";
 import { useRegisterStatistics } from "../hooks/useRegisterStatistics";
 
 function RegisterStatistics() {
-  const { setLeagueId } = useRegisterStatistics();
+  const { setLeagueId, league, isLoadingLeague } = useRegisterStatistics();
 
   return (
     <Box>
       <SelectLeague onChange={setLeagueId} />
+
+      <Flex mt="4" justifyContent="center">
+        {isLoadingLeague ? (
+          <CircularProgress isIndeterminate />
+        ) : (
+          <>{league ? <Box>rodada</Box> : null}</>
+        )}
+      </Flex>
     </Box>
   );
 }
